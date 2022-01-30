f83199afaa9a31699be160dddbe8f6f7c1b74930
Matheusss03 <ultra10vascaino@gmail.com>
2022-01-30 19:55:28 -0300

    Teste de commit.

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Um simples teste aaaa!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
