f92c7ecf93d89f7dfa69e1c14593a2bd523627bf
Matheusss03 <ultra10vascaino@gmail.com>
2022-03-10 01:40:26 -0300

    Atualizando os campos na tela de cadastro de usuário.

--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -4,12 +4,14 @@ import CustomInput from '../../components/CustomInput/CustomInput'
 import CustomButton from '../../components/CustomButton/CustomButton'
 import SocialSignInButtons from '../../components/SocialSignInButtons'
 import { useNavigation } from '@react-navigation/native'
+import {useForm} from 'react-hook-form'
+
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 const SignUpScreen = () => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordRepeat, setPasswordRepeat] = useState('')
+  const {control, handleSubmit, watch} = useForm();
+  const pwd = watch('senha')
 
   const navigation = useNavigation()
 
@@ -35,34 +37,59 @@ const SignUpScreen = () => {
         <Text style={styles.title}>Criar uma conta</Text>
 
         <CustomInput
-            placeholder="Login"
-            value={username}
-            setValue={setUsername}
+            name="nome"
+            control={control}
+            placeholder="Nome"
+            rules={{
+              required: 'Nome obrigatório',
+              minLength: {
+                value: 3,
+                message: 'Nome precisa ter no mínimo 3 caracteres',
+              },
+              maxLength: {
+                value: 24,
+                message: 'Nome pode ter no máximo 24 caracteres',
+              },
+            }}
           />
 
         <CustomInput
             placeholder="Email"
-            value={email}
-            setValue={setEmail}
+            control={control}
+            name="email"
+            rules={{
+              required: 'Email é obrigatório',
+              pattern: {value: EMAIL_REGEX, message: 'Email inválido'},
+            }}
           />
 
         <CustomInput
             placeholder="Senha"
-            value={password}
-            setValue={setPassword}
+            name="senha"
+            control={control}
+            rules={{
+              required: 'Senha obrigatória',
+              minLength: {
+                value: 8,
+                message: 'Senha precisa ter no mínimo 8 caracteres',
+              },
+            }}
             secureTextEntry
           />
 
         <CustomInput
             placeholder="Repita a senha"
-            value={passwordRepeat}
-            setValue={setPasswordRepeat}
+            control={control}
+            name="password-repeat"
             secureTextEntry
+            rules={{
+              validate: value => value === pwd || 'Senhas não são iguais',
+            }}
           />
 
         <CustomButton
             text="Cadastrar"
-            onPress={onRegisterPressed}
+            onPress={handleSubmit(onRegisterPressed)}
         />
 
         <Text style={styles.text}>
