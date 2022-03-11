9ab2a5b6bf9229e4de92bbb2169a81531f7c9fb4
Matheusss03 <ultra10vascaino@gmail.com>
2022-03-10 22:11:39 -0300

    Atualizando os campos da página de recuperação de senha.

--- src/screens/NewPasswordScreen/NewPasswordScreen.js
+++ src/screens/NewPasswordScreen/NewPasswordScreen.js
@@ -1,12 +1,12 @@
 import { View, Text, StyleSheet, ScrollView} from 'react-native'
-import React, {useState} from 'react'
+import React from 'react'
 import CustomInput from '../../components/CustomInput/CustomInput'
 import CustomButton from '../../components/CustomButton/CustomButton'
 import { useNavigation } from '@react-navigation/native'
+import { useForm } from 'react-hook-form'
 
 const NewPasswordScreen = () => {
-  const [code, setCode] = useState('')
-  const [newPassword, setnewPassword] = useState('')
+  const {control, handleSubmit} = useForm();
 
   const navigation = useNavigation()
 
@@ -21,28 +21,37 @@ const NewPasswordScreen = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View style={styles.root}>
-        <Text style={styles.title}>Crei uma nova senha</Text>
+        <Text style={styles.title}>Crie uma nova senha</Text>
 
         <CustomInput
             placeholder="Código"
-            value={code}
-            setValue={setCode}
+            control={control}
+            name="code"
+            rules={{required: 'Insira o código recebido no e-mail cadastrado'}}
           />
 
         <CustomInput
             placeholder="Nova senha"
-            value={newPassword}
-            setValue={setnewPassword}
+            control={control}
+            name="new_password"
+            secureTextEntry
+            rules={{
+              required: 'Senha obrigatória',
+              minLength: {
+                value: 8,
+                message: 'Senha precisa ter no mínimo 8 caracteres',
+              },
+            }}
           />
 
         <CustomButton
             text="Enviar"
-            onPress={onSendPressed}
+            onPress={handleSubmit(onSendPressed)}
         />
 
         <CustomButton
             text="Voltar ao login"
-            onPress={onSignInPress}
+            onPress={handleSubmit(onSignInPress)}
             type="TERTIARY"
         />
 
