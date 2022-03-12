e805096d7857642692cdc642b91f8e364e2b98e0
Matheusss03 <ultra10vascaino@gmail.com>
2022-03-12 15:46:49 -0300

    Configurando o AWS Amplify no app.

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 import Navigation from './src/navigation';
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+
+Amplify.configure(config)
 
 function App() {
+  //Auth.signOut()
   return (
     <SafeAreaView style={styles.root}>
       <Navigation/>
