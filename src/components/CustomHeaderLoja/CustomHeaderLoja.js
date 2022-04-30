8278daa97a2b7197f2aa9586ebb9083977a6935b
Matheusss03 <ultra10vascaino@gmail.com>
2022-04-30 17:08:50 -0300

    Criando o módulo de cabeçalho.

---
+++ src/components/CustomHeaderLoja/CustomHeaderLoja.js
@@ -0,0 +1,44 @@
+import { View, Text, Image, useWindowDimensions, StyleSheet } from 'react-native'
+import React from 'react'
+
+const cover = require("../../../assets/images/tenda.jpg")
+
+const dimensions = {
+    with: 900,
+    height: 450,
+  }
+
+const ratio = dimensions.height / dimensions.with
+
+function CustomHeaderLoja() {
+    const window = useWindowDimensions()
+
+    const width = window.width
+    const height = width * ratio
+
+  return (
+    <>
+      <Image style={{ width, height }} source={cover} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Monte Olimpo</Text>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "#6F4E37",
+      paddingVertical: 8,
+      marginBottom: 16,
+    },
+  
+    title: {
+      alignSelf: "center",
+      fontSize: 36,
+      fontWeight: "bold",
+      color: "#FFF",
+    },
+  })
+
+export default CustomHeaderLoja
