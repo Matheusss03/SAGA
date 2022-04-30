337feddf61b4a4dc006d54b310a6389443c4860b
Matheusss03 <ultra10vascaino@gmail.com>
2022-04-30 17:08:10 -0300

    Atualizando a tela da loja.

---
+++ src/screens/StoreScreen/StoreScreen.tsx
@@ -0,0 +1,33 @@
+import {View, Text, FlatList, StyleSheet, ListRenderItemInfo} from 'react-native'
+import React from 'react'
+
+import { Loja, storeList } from "../../../data/store"
+import CustomHeaderLoja from "../../components/CustomHeaderLoja/CustomHeaderLoja"
+import StoreItem from "../../components/StoreItem/StoreItem"
+import { SeparatorItem } from '../../components/SeparatorItem/SeparatorItem'
+
+const StoreScreen = () => {
+  function renderItem({ item }: ListRenderItemInfo<Loja>) {
+    return <StoreItem {...item}/>
+  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={CustomHeaderLoja}
+        ItemSeparatorComponent={SeparatorItem}
+        data={storeList}
+        keyExtractor={(item) => item.name}
+        renderItem={renderItem}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+})
+
+export default StoreScreen
