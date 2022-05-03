3207334a64b6132a62c435819a2898dfad363372
Matheusss03 <ultra10vascaino@gmail.com>
2022-05-03 07:05:09 -0300

    Criação da página de calendário.

--- src/screens/CalendarScreen/CalendarScreen.js
+++ src/screens/CalendarScreen/CalendarScreen.js
@@ -1,75 +1,134 @@
-import { View, TouchableOpacity, Text } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, Platform, View, TextInput, TouchableOpacity, Keyboard, ScrollView, SafeAreaView } from 'react-native'
 import React, {useState} from 'react'
-import {Agenda} from 'react-native-calendars'
-import {Card, Avatar} from 'react-native-paper'
-/*
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
-};
-*/
+
+import { DateSelectionCalendar } from 'react-native-easy-calendar'
+import Portugues from '../../../data/pt-br'
+import Task from './Task'
+
 const CalendarScreen = () => {
+  const [selectedDate, setSelectedDate] = React.useState('2022-05-03')
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
+  
   return (
-    <View>
-      <Text>CalendarScreen</Text>
-    </View>
-  )/*
-  const [items, setItems] = useState({})
+  <SafeAreaView style={styles.safe}>
+    <DateSelectionCalendar
+    locale={CustomLocale}
+    onSelectDate={setSelectedDate}
+    selectedDate={selectedDate}
+  />
+    <View style={styles.container}>
+        {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1
+          }}
+          keyboardShouldPersistTaps='handled'
+        >
 
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  };
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Tarefas do dia</Text>
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                    <Task text={item} /> 
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </View>
+        </View>
+          
+        </ScrollView>
 
-  const renderItem = (item) => {
-    return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
-        <Card>
-          <Card.Content>
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <Text>{item.name}</Text>
-              <Avatar.Text label="J" />
+        {/* Write a task */}
+        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+        <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput style={styles.input} placeholder={'Adicione uma tarefa'} value={task} onChangeText={text => setTask(text)} />
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
             </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-    );
-  };
-
-  return (
-    <View style={{flex: 1}}>
-      <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2017-05-16'}
-        renderItem={renderItem}
-      />
-    </View>
-  )*/
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+        
+      </View>
+    </SafeAreaView>
+  );
 }
+const CustomLocale = {
+  ...Portugues,
+  weekdaysMin: 'D_S_T_Q_Q_S_S'.split('_'),
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  safe: {
+    flex: 1,
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+});
 
 export default CalendarScreen
